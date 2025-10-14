8417c25b23ced3f25a3768f157f307abebda4cac
Elian <elian.enria1@gmail.com>
2025-10-13 21:00:16 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,15 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:5173', credentials: true }); //quitar en produccion
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://frontend-gv.vercel.app',
+      'https://xp-sales-vista.vercel.app/#',
+      'https://xp-sales-vista.vercel.app',
+    ],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
